7cda62d2dda4edc59853dae3cb353e4b9c65e529
compliment <nishkumar1@outlook.com>
2020-03-05 08:20:12 +0530

    patch site url for now

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     siteTitle: `University Department of Chemistry`,
     siteTitleAlt: `Department of Chemistry, BNMU.`,
     siteHeadline: `University Department of Chemistry, B. N. Mandal University.`,
-    siteUrl: `https://docbnmu.in`,
+    siteUrl: `https://new.docbnmu.in`,
     siteDescription: `Bhupendra Narayan Mandal University is at the district headquarters, Madhepura. It acts as an affiliating institution and offers undergraduate, graduate and PhD programs through its constituents, affiliated colleges, and institutes in Madhepura and nearby cities.`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
