a0bb982af67af31e948d3e824af960b81c723a7d
compliment <nishkumar1@outlook.com>
2020-03-27 19:15:09 +0530

    Freeze 1

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     siteUrl: `https://new.docbnmu.in`,
     siteDescription: `Bhupendra Narayan Mandal University is at the district headquarters, Madhepura. It acts as an affiliating institution and offers undergraduate, graduate and PhD programs through its constituents, affiliated colleges, and institutes in Madhepura and nearby cities.`,
     siteLanguage: `en`,
-    siteImage: `/banner.jpg`,
+    siteImage: `/banner.png`,
   },
   plugins: [
     {
