115e13b4f1e8644ed64b82f1c4f66fb110f2c456
compliment <nishkumar1@outlook.com>
2020-04-10 14:26:39 +0530

    Remove CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -82,7 +82,7 @@ module.exports = {
    },
     `gatsby-plugin-offline`,
     `gatsby-plugin-netlify`,
-    `gatsby-plugin-netlify-cms`,
+    //`gatsby-plugin-netlify-cms`,
     // `gatsby-plugin-webpack-bundle-analyser-v2`,
   ],
 }
